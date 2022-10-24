0a824cc9a00ee583f28c94c634298bdbc06c6736
Nikula Daniil <daniil.nikula@gmail.com>
2022-10-24 13:23:40 +0300

    Добавьте файлы проекта.

---
+++ Module 8 Task 2/DirectoryExtension.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_8_Task_2
+{
+    internal class DirectoryExtension
+    {
+        public static long CountSize(DirectoryInfo d)
+        {
+            long size = 0;
+            FileInfo[] fis = d.GetFiles();
+            foreach(FileInfo fi in fis)
+            {
+                size += fi.Length;
+            }
+
+            DirectoryInfo[] dis = d.GetDirectories();
+            foreach(DirectoryInfo di in dis)
+            {
+                size += CountSize(di);
+            }
+            return size;
+        }
+    }
+}
---
+++ Module 8 Task 2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Module_8_Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите путь:");
+            string path = Console.ReadLine();
+            DirectoryInfo di = new DirectoryInfo(path);
+            if (di.Exists)
+            {
+                Console.WriteLine("Размер: " + DirectoryExtension.CountSize(di) + " байт");
+            }
+            else
+            {
+                Console.WriteLine("Папки не существует");
+            }
+        }
+    }
+}
